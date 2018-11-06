734a652ee96a2f6d9638b6aa6561b4cf32d10cc4
ifschilling <ianfschilling@gmail.com>
2018-11-06 00:23:00 -0200

    update and debugging

--- Model/Associacoes/DisciplinaCurso.py
+++ Model/Associacoes/DisciplinaCurso.py
@@ -1,9 +1,17 @@
 class DisciplinaCurso(object):
 
-    __associacoes = []
+    __associacoes = {}
 
     def __init__(self, curso_id, disciplina_id):
         self.__curso_id = curso_id
         self.__disciplina_id = disciplina_id
 
-        DisciplinaCurso.__associacoes.append(self)
+        DisciplinaCurso.__associacoes = self
+
+    @property		
+    def curso_id(self):
+        return self.__curso_id
+        
+    @property
+    def disciplina_id(self):
+        return self.__disciplina_id
--- Model/Associacoes/UsuarioDisciplina.py
+++ Model/Associacoes/UsuarioDisciplina.py
@@ -1,8 +1,12 @@
 class UsuarioDisciplina(object):
-    __associacoes = []
-
     def __init__(self, disciplina_id, usuario_id):
         self.__disciplina_id = disciplina_id
         self.__usuario_id = usuario_id
 
-        UsuarioDisciplina.__associacoes.append(self)
+    @property		
+    def disciplina_id(self):
+        return self.__disciplina_id
+        
+    @property
+    def usuario_id(self):
+        return self.__usuario_id
